1a40fcc102e3776efae68942641f6a16091cb35a
GiverofMemory <78986433+GiverofMemory@users.noreply.github.com>
2023-04-23 01:24:35 +0000

    4-23-23 update add start.bat

---
+++ start.bat
@@ -0,0 +1,3 @@
+Rem this file is so you can replace word1 word2 etc with your own search strings.  You can also add more if you want, and remove the ones you aren't using.
+
+start mkp224o.exe word1 word2 word3
