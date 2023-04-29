f0abccf2cc83f3517b9e2aa717345fd45b1e94c3
GiverofMemory <78986433+GiverofMemory@users.noreply.github.com>
2023-04-29 07:28:55 +0000

    add priority for bat file

--- Filter and start files/filter.bat
+++ Filter and start files/filter.bat
@@ -1,3 +1,6 @@
-Rem . This file is to run your filters that you placed in the f.txt file
+Rem . This file is to run your filters that you placed in the f.txt file.  Priorities can be Normal, Idle, High, RealTime, BelowNormal, AboveNormal https://superuser.com/questions/620724/changing-windows-process-priority-via-command-line remember that the path 'C:\mkp224o\mkp224o.exe' will vary based on the location of your mkp224o.exe file.
 
-start mkp224o.exe -f f.txt
+Rem start mkp224o.exe -f f.txt
+
+start /b /wait powershell.exe -command "C:\mkp224o\mkp224o.exe -f f.txt;$prog = Get-Process -Name mkp224o;$prog.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::BelowNormal"
+
---
+++ Filter and start files/filterold.bat
@@ -0,0 +1,3 @@
+Rem . This file is to run your filters that you placed in the f.txt file
+
+start mkp224o.exe -f f.txt
